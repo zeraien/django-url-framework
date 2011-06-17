57a02a0df83022f52064a937b39c62fbe1963892
Dmitri Fedortchenko <d@angelhill.net>
2011-06-17 04:36:50 +0200

    fixed a bug in some decorators

--- django_url_framework/decorators.py
+++ django_url_framework/decorators.py
@@ -4,7 +4,7 @@ except ImportError:
     from django.utils.functional import update_wrapper, wraps  # Python 2.4 fallback.
 
 from django.contrib.auth import REDIRECT_FIELD_NAME
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.utils.decorators import available_attrs
 from django.utils.http import urlquote
 
