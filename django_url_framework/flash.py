9b5222ac128c515311afd26805f29bb1fb5be979
Dmitri Fedortchenko <d@angelhill.net>
2013-12-14 01:46:17 +0100

    minor refactoring

--- django_url_framework/flash.py
+++ django_url_framework/flash.py
@@ -3,7 +3,7 @@ import hashlib
 from django.utils.safestring import mark_safe
 
 class FlashMessage(object):
-    def __init__(self,message, is_error = False, kind = 'normal'):
+    def __init__(self, message, is_error=False, kind='normal'):
         self.message = message.encode('utf8')
         self.is_error = is_error
         self.kind = kind
@@ -43,38 +43,49 @@ class FlashManager(object):
     messages = property(_get_messages)
     
     def has_messages(self):
-        return len(self)>0
+        return len(self) > 0
     
     def clear(self):
         if self.SESSION_KEY in self.request.session:
             del(self.request.session[self.SESSION_KEY])
             self.request.session.save()
+
     def get_and_clear(self):
         messages = self.messages
         self.clear()
         return messages
+
     def __nonzero__(self):
         return len(self.messages) > 0
+
     def __iter__(self):
         return iter(self.messages)
+
     def __len__(self):
         return len(self.messages)
+
     def __getitem__(self, key):
         return self.messages[key]
     
     def append_error(self, msg):
-        self.append(msg, 'error')
+        self.append(msg=msg, msg_type='error')
         
-    def append(self, msg, msg_type = 'normal'):
-        new_message = FlashMessage( **{'message':msg, 'kind': msg_type, 'is_error':msg_type=='error'})
+    def append(self, msg, msg_type='normal'):
+
+        new_message = FlashMessage(**{'message': msg, 'kind': msg_type, 'is_error': msg_type == 'error'})
         new_hash = new_message.hash()
+
         for message in self.messages:
-            if message.hash()==new_hash:
+            if message.hash() == new_hash:
                 return
-        self.messages.append( new_message )
+
+        self.messages.append(new_message)
         self.request.session[self.SESSION_KEY] = [m.json_ready() for m in self.messages]
-    def set(self, msg, msg_type = 'normal'):
-        self.append(msg, msg_type)
+
+    def set(self, msg, msg_type='normal'):
+        self.clear()
+        self.append(msg=msg, msg_type=msg_type)
+
     def error(self, msg):
-        self.append(msg, 'error')
-    append_error = error
+        self.clear()
+        self.append(msg=msg, msg_type='error')
