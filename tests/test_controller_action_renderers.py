45cee934b5d46e515c75b63dfd17ef207b487113
Dimo <d@angelhill.net>
2020-04-10 08:20:03 +0530

    more unit tests

--- tests/test_controller_action_renderers.py
+++ tests/test_controller_action_renderers.py
@@ -13,14 +13,15 @@ settings.configure()
 
 class TestController(unittest.TestCase):
 
-    def _request_and_test(self, ControllerKlass, action_name, expected_response, **headers):
+    def _request_and_test(self, ControllerKlass, action_name, expected_response=None, status_code=200, **headers):
         with self.subTest(**headers):
             rf =  RequestFactory()
             request = rf.get('/test/json/', **headers)
 
             response = ControllerKlass(site=None, request=request, helper_class=None, url_params=None)._call_action(action_name)
-            self.assertEquals(response.status_code, 200)
-            self.assertEquals(response.content.decode('utf8').strip(), expected_response.strip())
+            self.assertEquals(response.status_code, status_code)
+            if expected_response:
+                self.assertEquals(response.content.decode('utf8').strip(), expected_response.strip())
             return response
 
     def test_auto_json_yaml_str(self):
@@ -49,75 +50,87 @@ class TestController(unittest.TestCase):
     def test_auto_decorator_with_params(self):
         expected = {'ab':"C",1:"2",None:False}
 
-        def _run_test(expect, **kwargs):
-            class TestDecoratorWithParamsController(ActionController):
-                @auto(yaml_default_flow_style=True)
-                def test_action(self, request):
-                    return expected
-            self._request_and_test(TestDecoratorWithParamsController, "test_action",
-                                   HTTP_ACCEPT="application/yaml",
-                                   expected_response=expect)
-
-        _run_test(yaml.dump(expected,default_flow_style=True))
+        class TestDecoratorWithParamsController(ActionController):
+            @auto(yaml_default_flow_style=True)
+            def test_action(self, request):
+                return expected
+        self._request_and_test(TestDecoratorWithParamsController, "test_action",
+                               HTTP_ACCEPT="application/yaml",
+                               expected_response=yaml.dump(expected,default_flow_style=True))
 
     def test_json_decorator(self):
         expected = {'ab':"C",1:"2",None:False}
 
-        def _run_test(expect, **kwargs):
-            class TestJSONDecoratorController(ActionController):
-                @json_action()
-                def test_action(self, request):
-                    return expected
-            self._request_and_test(TestJSONDecoratorController, "test_action", expected_response=expect)
+        class TestJSONDecoratorController(ActionController):
+            @json_action()
+            def test_action(self, request):
+                return expected
+        self._request_and_test(TestJSONDecoratorController, "test_action", expected_response=json.dumps(expected))
 
-        _run_test(json.dumps(expected))
 
-    def test_before_filter(self):
+    def test_before_filter_redirect(self):
         returned = {"foo":"bar"}
 
-        def _run_test(expect, **kwargs):
-            class TestPrintController(ActionController):
-                def _before_filter(self, request):
-                    return {"add":123}
-                @json_action()
-                def test_action(self, request):
-                    return returned
-            self._request_and_test(TestPrintController, "test_action", expected_response=expect)
+        class TestPrintController(ActionController):
+            def _before_filter(self, request):
+                return self._go(to_url="/baz/")
 
-        _run_test(json.dumps({"foo":"bar", "add":123}))
+            @json_action()
+            def test_action(self, request):
+                return returned
+        response = self._request_and_test(TestPrintController, "test_action", status_code=302)
+        self.assertEquals(response['Location'], "/baz/")
+
+    def test_before_filter_none(self):
+        returned = {"foo":"bar"}
+
+        class TestPrintController(ActionController):
+            def _before_filter(self, request):
+                return None
+            @json_action()
+            def test_action(self, request):
+                return returned
+        self._request_and_test(TestPrintController, "test_action", expected_response=json.dumps(returned))
+
+    def test_before_filter_dict(self):
+        returned = {"foo":"bar"}
+
+        class TestPrintController(ActionController):
+            def _before_filter(self, request):
+                return {"add":123}
+            @json_action()
+            def test_action(self, request):
+                return returned
+        self._request_and_test(TestPrintController, "test_action", expected_response=json.dumps({"foo":"bar", "add":123}))
 
     def test_print(self):
         expected = [1,2,3,4,5]
 
-        def _run_test(expect, **kwargs):
+        def _run_test(input, expect, **kwargs):
             class TestPrintController(ActionController):
                 def test_action(self, request):
-                    return self._print(expected)
+                    return self._print(input)
             self._request_and_test(TestPrintController, "test_action", expected_response=expect)
 
-        _run_test(str(expected))
+        _run_test(expected, str(expected))
+        _run_test("Bajs", "Bajs")
+        _run_test({"a":"b"}, str({"a":"b"}))
 
     def test_as_yaml(self):
-        expected = {'ab':"C",1:"2",None:False}
+        input = {'ab':"C",1:"2",None:False}
 
-        def _run_test(expect, **kwargs):
-            class TestAsYamlController(ActionController):
-                def test_action(self, request):
-                    return self._as_yaml(expected, default_flow_style=True)
-            self._request_and_test(TestAsYamlController, "test_action", expected_response=expect)
-
-        _run_test(yaml.dump(expected, default_flow_style=True))
+        class TestAsYamlController(ActionController):
+            def test_action(self, request):
+                return self._as_yaml(input, default_flow_style=True)
+        self._request_and_test(TestAsYamlController, "test_action", expected_response=yaml.dump(input, default_flow_style=True))
 
     def test_as_json(self):
-        expected = {'ab':"C",1:"2",None:False}
+        input = {'ab':"C",1:"2",None:False}
 
-        def _run_test(expect, **kwargs):
-            class TestAsJsonController(ActionController):
-                def test_action(self, request):
-                    return self._as_json(expected)
-            self._request_and_test(TestAsJsonController, "test_action", expected_response=expect)
-
-        _run_test(json.dumps(expected))
+        class TestAsJsonController(ActionController):
+            def test_action(self, request):
+                return self._as_json(input)
+        self._request_and_test(TestAsJsonController, "test_action", expected_response=json.dumps(input))
 
     def test_redirect_action(self):
 
@@ -146,39 +159,121 @@ class TestController(unittest.TestCase):
     def test_yaml_decorator(self):
         expected = {'ab':"C",1:"2",None:False}
 
-        def _run_test(expect, **kwargs):
-            class TestYamlDecoratorController(ActionController):
-                yaml_default_flow_style=True
-                @yaml_action()
-                def test_action(self, request):
-                    return expected
-            self._request_and_test(TestYamlDecoratorController, "test_action", expected_response=expect)
-
-        _run_test(yaml.dump(expected,default_flow_style=True))
+        class TestYamlDecoratorController(ActionController):
+            yaml_default_flow_style=True
+            @yaml_action()
+            def test_action(self, request):
+                return expected
+        self._request_and_test(TestYamlDecoratorController, "test_action", expected_response=yaml.dump(expected,default_flow_style=True))
 
     def test_yaml_decorator_with_flow_style(self):
         expected = {'ab':"C",1:"2",None:False}
 
-        def _run_test(expect, **kwargs):
-            class TestYamlWithFlowController(ActionController):
-                @yaml_action(default_flow_style=True)
-                def test_action(self, request):
-                    return expected
-            self._request_and_test(TestYamlWithFlowController, "test_action", expected_response=expect)
-
-        _run_test(yaml.dump(expected,default_flow_style=True))
+        class TestYamlWithFlowController(ActionController):
+            @yaml_action(default_flow_style=True)
+            def test_action(self, request):
+                return expected
+        self._request_and_test(TestYamlWithFlowController, "test_action", expected_response=yaml.dump(expected,default_flow_style=True))
 
     def test_yaml_decorator_with_flow_style_false(self):
-        expected = {'ab':"C",1:"2",None:False}
+        input = {'ab':"C",1:"2",None:False}
 
-        def _run_test(expect, **kwargs):
-            class TestYamlDecoWithFalseFlowController(ActionController):
-                @yaml_action(default_flow_style=False)
-                def test_action(self, request):
-                    return expected
-            self._request_and_test(TestYamlDecoWithFalseFlowController, "test_action", expected_response=expect)
+        class TestYamlDecoWithFalseFlowController(ActionController):
+            @yaml_action(default_flow_style=False)
+            def test_action(self, request):
+                return input
+        self._request_and_test(TestYamlDecoWithFalseFlowController, "test_action", expected_response=yaml.dump(input,default_flow_style=False))
 
-        _run_test(yaml.dump(expected,default_flow_style=False))
+    def test_after_filter(self):
+        input = {'ab':"C",1:"2",None:False}
+        after = {'c':'z'}
+        class TestAfterFilterController(ActionController):
+            def _after_filter(self, request):
+                return after
+            @json_action()
+            def test_action(self, request):
+                return input
 
-if __name__ == '__main__':
-    unittest.main()
+        copied = input.copy()
+        copied.update(after)
+
+        self._request_and_test(
+            TestAfterFilterController,
+            "test_action",
+            expected_response=json.dumps(copied)
+        )
+
+    def test_after_filter_can_access_context(self):
+        """
+        This verifies that `_after_filter` is run, that it has access to the
+        context that was returned by an `action`, and that `_after_filter` can modify
+        the context before returning it to the client.
+
+        after_filter takes value `foo` from our dictionary,
+        and assigns it to key `bar`. It should also replace the original
+        `foo` value with `bazinga`
+
+        :return:
+        """
+        input = {'foo':"123"}
+        class TestAfterFilterContextController(ActionController):
+            def _after_filter(self, request):
+                after = {
+                    'bar': self._template_context['foo'],
+                    'foo': 'bazinga'
+                }
+                return after
+            @json_action()
+            def test_action(self, request):
+                return input
+        self._request_and_test(
+            TestAfterFilterContextController,
+            "test_action",
+            expected_response=json.dumps({"foo":'bazinga',"bar":"123"}))
+
+
+    def test_tuple_response_status_code(self):
+
+        expected = "HAIHAIHAI"
+        class TupleController(ActionController):
+            @json_action()
+            def three_three(self, request):
+                return expected, 333
+        rf =  RequestFactory()
+        request = rf.get('/three_three/')
+        controller = TupleController(site=None, request=request, helper_class=None, url_params=None)
+        response = controller._call_action('three_three')
+        self.assertEquals(response.status_code, 333)
+        self.assertEquals(response.content.decode('utf8'), json.dumps(expected))
+
+    def test_as_json_tuple_response_status_code(self):
+
+        expected = "HAIHAIHAI"
+        class TupleController(ActionController):
+            def three_three(self, request):
+                return self._as_json(expected), 333
+        self._request_and_test(TupleController, "three_three", json.dumps(expected), 333)
+
+    def test_as_json_param_response_status_code(self):
+
+        expected = "HAIHAIHAI"
+        class TupleController(ActionController):
+            def three_three(self, request):
+                return self._as_json(expected, status_code=333)
+        self._request_and_test(TupleController, "three_three", json.dumps(expected), 333)
+
+    def test_param_tuple_status_code(self):
+
+        expected = "HAIHAIHAI"
+        class TupleController(ActionController):
+            def three_three(self, request):
+                return self._print(expected),334
+        self._request_and_test(TupleController, "three_three", expected, 334)
+
+    def test_as_json_param_and_tuple_response_status_code(self):
+
+        expected = "HAIHAIHAI"
+        class TupleController(ActionController):
+            def three_three(self, request):
+                return self._as_json(expected, status_code=333), 444
+        self._request_and_test(TupleController, "three_three", json.dumps(expected), 333)
