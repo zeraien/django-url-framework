2a2dc621970c2cc073e5f801f345e598c026d33d
Dimo <d@angelhill.net>
2020-04-10 06:02:51 +0530

    change ugettext to gettext

--- django_url_framework/apps.py
+++ django_url_framework/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy
+from django.utils.translation import gettext_lazy
 
 
 class URLFrameworkAppConfig(AppConfig):
     name = 'django_url_framework'
-    verbose_name = ugettext_lazy("Django URL Framework")
+    verbose_name = gettext_lazy("Django URL Framework")
