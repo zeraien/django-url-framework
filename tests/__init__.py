ff4a73805f7cd407638cfad6d7b6258302887cfe
Dimo <d@angelhill.net>
2020-04-11 07:48:38 +0530

    some basic django configuration added for the unit tests to work, and also a placeholder template.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,17 @@
 import unittest
+from os.path import dirname
+
+from django.conf import settings
+settings.configure(
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.dummy.TemplateStrings',
+            'APP_DIRS': False,
+            'DIRS': [dirname(__file__)]
+        },
+    ]
+
+)
 
 if __name__ == '__main__':
     unittest.main()
